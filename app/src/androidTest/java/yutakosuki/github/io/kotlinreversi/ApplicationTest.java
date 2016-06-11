e74e5a6aa5702cc880d36c4990a82fcd41eccf6f
YutaKosuki <bambiyuta@gmail.com>
2016-06-11 14:55:29 +0900

    initial commit

---
+++ app/src/androidTest/java/yutakosuki/github/io/kotlinreversi/ApplicationTest.java
@@ -0,0 +1,13 @@
+package yutakosuki.github.io.kotlinreversi;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/yutakosuki/github/io/kotlinreversi/KotlinReversi.java
@@ -0,0 +1,13 @@
+package yutakosuki.github.io.kotlinreversi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KotlinReversi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kotlin_reversi);
+    }
+}
