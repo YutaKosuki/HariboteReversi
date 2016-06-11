3805963469f862a23c1b0b72ba184283c8f74a44
YutaKosuki <bambiyuta@gmail.com>
2016-06-11 15:08:06 +0900

    convert kotlin

---
+++ app/src/main/kotlin/yutakosuki/github/io/kotlinreversi/KotlinReversi.kt
@@ -0,0 +1,12 @@
+package yutakosuki.github.io.kotlinreversi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinReversi : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_reversi)
+    }
+}
